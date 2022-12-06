86002e11bd59dec95aec2bb15b918b44bedcd0e3
Mr. Galvan <112872430+Juang1908@users.noreply.github.com>
2022-12-05 19:47:30 -0600

    updated HTML

--- game page/game.js
+++ game page/game.js
@@ -1,7 +1,13 @@
+var now = dayjs().format("dddd, MMMM D, YYYY h:mm A")
+  $("#currentDay").text(now)
+console.log(now)
+
+
+
 const options = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY',
+		'X-RapidAPI-Key': 'ec19531030msh549c1ba3d1e32c2p1c9159jsnab8641a11590',
 		'X-RapidAPI-Host': 'livescore6.p.rapidapi.com'
 	}
 };
