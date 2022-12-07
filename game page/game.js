7b1c1c4e9ba0ac1c4bc4e88a5e316c03e853d47e
Mr. Galvan <112872430+Juang1908@users.noreply.github.com>
2022-12-06 19:47:58 -0600

    HTML

--- game page/game.js
+++ game page/game.js
@@ -1,10 +1,15 @@
+const scoresEl = document.getElementById("scores");
+const matchEl = document.getElementById("match");
+const lineupsEl = document.getElementById("lineups");
+const highlightsEl = document.getElementById("highlights")
+
 var now = dayjs().format("dddd, MMMM D, YYYY h:mm A")
   $("#currentDay").text(now)
 console.log(now)
 
 
-
-const options = {
+// Lineups
+const lineups = {
 	method: 'GET',
 	headers: {
 		'X-RapidAPI-Key': 'ec19531030msh549c1ba3d1e32c2p1c9159jsnab8641a11590',
@@ -12,7 +17,27 @@ const options = {
 	}
 };
 
-fetch('https://livescore6.p.rapidapi.com/matches/v2/list-live?Category=soccer&Timezone=-7', options)
+fetch('https://livescore6.p.rapidapi.com/matches/v2/get-lineups?Eid=702093&Category=soccer', lineups)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
+    console.log(lineups);
+
+
+
+
+
+    
+// Scoreboard
+const scoreboard = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'ec19531030msh549c1ba3d1e32c2p1c9159jsnab8641a11590',
+		'X-RapidAPI-Host': 'livescore6.p.rapidapi.com'
+	}
+};
+
+fetch('https://livescore6.p.rapidapi.com/matches/v2/get-scoreboard?Eid=702093&Category=soccer', scoreboard)
 	.then(response => response.json())
 	.then(response => console.log(response))
 	.catch(err => console.error(err));
