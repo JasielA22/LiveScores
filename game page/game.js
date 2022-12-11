9c4ca0720fb72d8a78bed772c12533792c4e6bff
Mr. Galvan <112872430+Juang1908@users.noreply.github.com>
2022-12-11 09:04:58 -0600

    rearranged js and added img

--- game page/game.js
+++ game page/game.js
@@ -1,4 +1,5 @@
 const homeTEl = document.getElementById("team1");
+const h1ImgEl = document.getElementById("img1");
 const visitTEl = document.getElementById("team2");
 const lineupsEl = document.getElementById("lineups");
 const highlightsEl = document.getElementById("highlights")
@@ -7,38 +8,48 @@ var now = dayjs().format("dddd, MMMM D, YYYY h:mm A")
   $("#currentDay").text(now)
 console.log(now)
 
-// Match Info 
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'ec19531030msh549c1ba3d1e32c2p1c9159jsnab8641a11590',
-		'X-RapidAPI-Host': 'livescore6.p.rapidapi.com'
-	}
-};
 
-var teamsData;
-var teamInfo1;
-var teamInfo2;
-
-
-fetch('https://livescore6.p.rapidapi.com/matches/v2/list-by-date?Category=soccer&Date=20221210&Timezone=-7', options)
-	.then(response => response.json())
-	.then(response => {
-   teamsData = response.Stages[0]
-    teamInfo1 = teamsData.Events[0].T1[0]
-    teamInfo2 = teamsData.Events[0].T2[0]
-    console.log(teamInfo1)
-    console.log(teamInfo2)
-    console.log(response)
-
-    homeTEl.textContent = teamInfo1;
-    visitTEl.textContent = teamInfo2;
-    for (var i = 0; i < teamsData.length; i++){
-      // homeTEl.textContent = teamInfo1;
-      // visitTEl.textContent = teamInfo2;
-    }
-
-  })
-	.catch(err => console.error(err));
   
- 
+
+function getData(){
+    fetch('https://livescore6.p.rapidapi.com/matches/v2/list-by-date?Category=soccer&Date=20221210&Timezone=-7',{
+        method: 'GET',
+        headers: { 'X-RapidAPI-Key': 'ec19531030msh549c1ba3d1e32c2p1c9159jsnab8641a11590',
+        'X-RapidAPI-Host': 'livescore6.p.rapidapi.com'}
+    })
+    .then(response => response.json().then(data => {
+        var teamsData =data["Stages"][0]["Events"][1];
+
+        var teamInfo1 = teamsData["T1"][0]["Nm"];
+        var team1img = teamsData["T1"][0]["Img"];
+        var teamInfo2 = teamsData["T2"][0]["Nm"];
+        console.log(response)
+        console.log(teamsData);
+        console.log(team1img);
+        console.log(teamInfo1);
+        console.log(teamInfo2);
+
+        homeTEl.textContent = teamInfo1;
+        // h1ImgEl.textContent = team1img;
+        visitTEl.textContent = teamInfo2;
+
+
+        for(var i = 0; i < teamsData; i++){
+        // homeTEl.textContent = teamInfo1;
+        // visitTEl.textContent = teamInfo2;
+        }
+    }))
+    .catch(err => console.error(err));
+
+
+
+}
+
+// call function
+getData();
+
+
+// var callstring = Data1.Img;
+// $("#team1").attr("src", "https://lsm-static-prod.livescore.com/medium/" + callstring);'''
+// https://lsm-static-prod.livescore.com/medium/
+// this part is what is needed before the Img string to be able to display
