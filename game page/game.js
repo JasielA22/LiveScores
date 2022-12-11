b441a74961ba7a34d1d08e0d5e40c647c5d5bb67
Mr. Galvan <112872430+Juang1908@users.noreply.github.com>
2022-12-11 09:40:41 -0600

    added video highlights on js

--- game page/game.js
+++ game page/game.js
@@ -21,16 +21,16 @@ function getData(){
         var teamsData =data["Stages"][0]["Events"][1];
 
         var teamInfo1 = teamsData["T1"][0]["Nm"];
-        var team1img = teamsData["T1"][0]["Img"];
+      
         var teamInfo2 = teamsData["T2"][0]["Nm"];
         console.log(response)
         console.log(teamsData);
-        console.log(team1img);
+        
         console.log(teamInfo1);
         console.log(teamInfo2);
 
         homeTEl.textContent = teamInfo1;
-        // h1ImgEl.textContent = team1img;
+        
         visitTEl.textContent = teamInfo2;
 
 
@@ -48,8 +48,24 @@ function getData(){
 // call function
 getData();
 
+function getAllData() {
+  fetch('https://free-football-soccer-videos.p.rapidapi.com/',{
+method: 'GET',
+headers: {
+  'X-RapidAPI-Key': 'ec19531030msh549c1ba3d1e32c2p1c9159jsnab8641a11590',
+  'X-RapidAPI-Host': 'free-football-soccer-videos.p.rapidapi.com'
+}
+})
+.then(response => response.json().then(data => {
+  var teamsHigh1 =data[7]["videos"][0]["embed"];
 
-// var callstring = Data1.Img;
-// $("#team1").attr("src", "https://lsm-static-prod.livescore.com/medium/" + callstring);'''
-// https://lsm-static-prod.livescore.com/medium/
-// this part is what is needed before the Img string to be able to display
+  console.log(teamsHigh1);
+
+  highlightsEl.innerHTML = teamsHigh1;
+
+}))
+
+.catch(err => console.error(err));
+
+}
+getAllData();
