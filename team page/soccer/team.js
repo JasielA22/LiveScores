589b4bd1fa29db299a800a1eb699a9d6a14a950e
saedcameron <cameronsaed@gmail.com>
2022-12-12 18:48:09 -0600

    Commmited changes to page

--- team page/soccer/team.js
+++ team page/soccer/team.js
@@ -12,58 +12,14 @@ var topTeams
 fetch('https://livescore6.p.rapidapi.com/matches/v2/list-by-league?Category=soccer&Ccd=world-cup&Scd=group-b&Timezone=-7', options)
 	.then(response => response.json())
 	.then(response => {
-		topTeams = response.Stages[0].LeagueTable.L[0].Tables[0].team[0]
+		topTeams = response.Stages[0].LeagueTable.L[0].Tables[0]
 		console.log(topTeams)
 		for (var i = 0; i < topTeams.length; i++) {
-			current = topTeams[i]
+			current = topTeams.team[i]
 			console.log(`Team Name: ${current.Tnm}`)
 			console.log(`Rank: ${current.rnk}`)
 		}
 	})
 	.catch(err => console.error(err));
 
-var topTeams
-
-
-fetch('https://livescore6.p.rapidapi.com/matches/v2/list-by-league?Category=soccer&Ccd=world-cup&Scd=group-b&Timezone=-7', options)
-	.then(response => response.json())
-	.then(response => {
-		topTeams = response.Stages[0].LeagueTable.L[0].Tables[0].team[1]
-		console.log(topTeams)
-		for (var i = 0; i < topTeams.length; i++) {
-			current = topTeams[i]
-			console.log(`Team Name: ${current.Tnm}`)
-			console.log(`Rank: ${current.rnk}`)
-		}
-	})
-	.catch(err => console.error(err));
-
-var topTeams
-
-
-fetch('https://livescore6.p.rapidapi.com/matches/v2/list-by-league?Category=soccer&Ccd=world-cup&Scd=group-b&Timezone=-7', options)
-	.then(response => response.json())
-	.then(response => {
-		topTeams = response.Stages[0].LeagueTable.L[0].Tables[0].team[2]
-		console.log(topTeams)
-		for (var i = 0; i < topTeams.length; i++) {
-			current = topTeams[i]
-			console.log(`Team Name: ${current.Tnm}`)
-			console.log(`Rank: ${current.rnk}`)
-		}
-	})
-	.catch(err => console.error(err));
-
-fetch('https://livescore6.p.rapidapi.com/matches/v2/list-by-league?Category=soccer&Ccd=world-cup&Scd=group-b&Timezone=-7', options)
-	.then(response => response.json())
-	.then(response => {
-		topTeams = response.Stages[0].LeagueTable.L[0].Tables[0].team[3]
-		console.log(topTeams)
-		for (var i = 0; i < topTeams.length; i++) {
-			current = topTeams[i]
-			console.log(`Team Name: ${current.Tnm}`)
-			console.log(`Rank: ${current.rnk}`)
-		}
-	})
-	.catch(err => console.error(err));
 
